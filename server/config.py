47d19af3aeb4fd0b215518720389f71297375308
Josh Liu <bvup66@gmail.com>
2021-03-10 10:54:09 -0800

    setup db with a sample user table

--- server/config.py
+++ server/config.py
@@ -1,3 +1,8 @@
 import os
+from flask_sqlalchemy import SQLAlchemy
 
-TEAM_NAME = os.environ['TEAM_NAME']
+TEAM_NAME = os.environ.get('TEAM_NAME')
+
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123456@localhost:5432/postgres"
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+db = SQLAlchemy()
