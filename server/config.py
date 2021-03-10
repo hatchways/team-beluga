2dc1a211e848f91d718024b884f39e790c257ef3
Josh Liu <bvup66@gmail.com>
2021-03-10 12:09:52 -0800

    update .gitignore

--- server/config.py
+++ server/config.py
@@ -2,7 +2,6 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 
 TEAM_NAME = os.environ.get('TEAM_NAME')
-
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123456@localhost:5432/postgres"
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 db = SQLAlchemy()
