c5894edaffb8336d37c6e629c185abf1903e733b
fangyu3 <tofang23@gmail.com>
2021-03-10 17:21:10 -0500

    Adding appointment model
    Updating folder structure

--- server/app.py
+++ server/app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 import config
 from config import db
-from server.model.users import Users
 from flask_migrate import Migrate
-
-
+import model
 app = Flask(__name__)
 app.config.from_object(config)
 db.init_app(app)
