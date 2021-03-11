a17ae32e008c9663347eee5cb007bca18bc12cf6
Josh Liu <bvup66@gmail.com>
2021-03-10 18:39:36 -0800

    delete team member arranged .env & config.py

--- server/config.py
+++ server/config.py
@@ -1,7 +1,6 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
 
-TEAM_NAME = os.environ.get('TEAM_NAME')
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 db = SQLAlchemy()
