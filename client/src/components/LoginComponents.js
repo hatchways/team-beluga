73a196b331537983c3af36bb75ef26ed3376878b
Josh Liu <bvup66@gmail.com>
2021-03-14 00:14:09 -0800

    move oauth clientId into .env

--- client/src/components/LoginComponents.js
+++ client/src/components/LoginComponents.js
@@ -102,9 +102,11 @@ function LoginComponent(props) {
 
 function GoogleLoginBtn(props) {
     const classes = useStyles();
+    const clientId = process.env.REACT_APP_OAUTH_CLIENT_ID; 
+    console.log(clientId);
     return (
         <GoogleLogin
-            clientId="802130452785-qc5dp590pd2qs6opc6udqa8qt6ofdihl.apps.googleusercontent.com"
+            clientId={clientId}
             render={renderProps => (
                 <Button className={classes.btnOrange} onClick={renderProps.onClick}>
                     <svg aria-hidden="true" focusable="false" height="16" width="16" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512" className="svg-inline--fa fa-google fa-w-16 fa-5x"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
