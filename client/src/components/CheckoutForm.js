0abf6c171b8dbf6036353c29f0f5082c025f178f
Josh Liu <bvup66@gmail.com>
2021-03-23 03:04:41 -0700

    strip BE api & FE framework

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
-
 import CardSection from './CardSection';
 
-function stripePaymentMethodHandler(result) {
+
+function stripeCheckoutHandler(result) {
     if (result.error) {
-        // Show error in payment form
+
     } else {
-        // Otherwise send paymentMethod.id to your server (see Step 4)
-        fetch('/pay', {
+        fetch('/create-subscription', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
+                priceId: 'price_1IY2oXFSBPZAgVJdLwijjbRb',
                 payment_method_id: result.paymentMethod.id,
+                email: 'testemail@email.com'
             })
         }).then(function (result) {
             // Handle server response (see Step 4)
@@ -24,26 +25,11 @@ function stripePaymentMethodHandler(result) {
     }
 }
 
-function stripeCheckoutHandler(result) {
-    fetch('/create-checkout-session', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            priceId: 'price_1IY2oXFSBPZAgVJdLwijjbRb',
-            payment_method_id: result.paymentMethod.id
-        })
-    }).then(function (result) {
-        // Handle server response (see Step 4)
-        result.json().then(function (json) {
-            //handleServerResponse(json);
-            console.log(json);
-        })
-    });
-}
-
 export default function CheckoutForm() {
     const stripe = useStripe();
     const elements = useElements();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
 
     const handleSubmit = async (event) => {
         // We don't want to let default form submission happen here,
@@ -61,21 +47,21 @@ export default function CheckoutForm() {
             card: elements.getElement(CardElement),
             billing_details: {
                 // Include any additional collected billing details.
-                name: 'Jenny Rosen',
+                name: name,
+                email: email
             },
         });
         console.log(result);
         //stripePaymentMethodHandler(result);
         stripeCheckoutHandler(result);
-    };    
-
+    };
 
     return (
         <form onSubmit={handleSubmit}>
             <CardSection />
             <button type="submit" disabled={!stripe}>
-                Submit Payment
-      </button>
+                Subscribe
+            </button>
         </form>
     );
 }
--- client/src/pages/StripeCheckout.js
+++ client/src/pages/StripeCheckout.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 import CheckoutForm from '../components/CheckoutForm';
-
-
-
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
