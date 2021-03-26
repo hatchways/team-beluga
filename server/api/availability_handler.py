77d9792730b81187343004d826f976e9fe31b79a
Josh Liu <bvup66@gmail.com>
2021-03-26 02:17:46 -0700

    update branch

--- server/api/availability_handler.py
+++ server/api/availability_handler.py
@@ -5,6 +5,7 @@ from model.model import Users
 import datetime
 from tzlocal import get_localzone
 from utils.auth.middleware import check_token
+import calendar
 
 
 availability_handler = Blueprint('availability_handler', __name__)
@@ -13,20 +14,33 @@ availability_handler = Blueprint('availability_handler', __name__)
 @check_token
 def get_calendar_availability(id):
     user = Users.query.filter_by(id=id).first()
+    dayStart = user.available_time.split(',')[0]
+    dayEnd = user.available_time.split(',')[1]
 
     # Instantiate google client with user access_token and refresh_token
     google_client = GoogleClient(access_token=user.access_token, refresh_token=user.refresh_token)
 
-    # TODO: Update timeMin & timeMax from time obtained from frontend
-    timeMin = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    timeMax= datetime.datetime.utcnow().replace(hour=23, minute=59,second=59).isoformat() + 'Z'
+    yearMonth = request.args.get('ym')
+    print(yearMonth)
+    if yearMonth is None or yearMonth == "":
+        # TODO: Update timeMin & timeMax from time obtained from frontend
+        timeMin = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+        last_day = calendar.monthrange(datetime.datetime.utcnow().year, datetime.datetime.utcnow().month)[1]
+        timeMax = datetime.datetime.utcnow().replace(day=last_day, hour=23, minute=59,second=59).isoformat() + 'Z'
+    else:
+        format_yearMonth = datetime.datetime.fromisoformat(yearMonth.replace('Z', ""))
+        last_day = calendar.monthrange(format_yearMonth.year, format_yearMonth.month)[1]
+        timeMin = format_yearMonth.replace(day=1, hour=0, minute=0, second=0).isoformat() + 'Z'
+        timeMax = format_yearMonth.replace(day=last_day, hour=23, minute=59, second=59).isoformat() + 'Z'
 
     # Get local timezone of user
-    timezone = get_localzone()
+    timezone = get_localzone()  # TODO: Get user set timezone from DB
     
     return jsonify({
-            "busy":google_client.get_user_calendar_info(timeMin=timeMin, timeMax=timeMax, timezone = timezone)}
-        ), 200
+        "busy": google_client.get_user_calendar_info(timeMin=timeMin, timeMax=timeMax, timezone=timezone),
+        "dayStart": dayStart,
+        "dayEnd": dayEnd
+    }), 200
 
 
 
